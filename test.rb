cd56803dc3f2cdca76b70c76fe2ed0c706f42305
George Gosieski <george.js.gosieski@gmail.com>
2015-03-13 23:07:40 -0700

    ITS THE ERIC ANDRE SHOWWWWWWWW

--- test.rb
+++ test.rb
@@ -1,3 +1,12 @@
+def itWorks (*kelvin)
+  kelvin.each {|kelvin| puts "It works, #{kelvin}!"}
+end
+
+
 puts 1+1
 print "Hi George"
 print "Lovely"
+
+itWorks("Kelvin", "Melvin", "Belvin", "Morpheous", "Orifice", "Walruses")
+
+
